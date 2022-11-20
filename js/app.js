cec921604da8d251337575f07e0f7f08733f1a0e
OnlyDropped <asheburenkov@yandex.ru>
2022-11-20 04:17:36 +0300

    added catalog.html and card.html

--- js/app.js
+++ js/app.js
@@ -1,3 +1,33 @@
+ $('.card-slider__big').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.card-slider__thumb'
+});
+$('.card-slider__thumb').slick({
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  asNavFor: '.card-slider__big',
+  dots: false,
+  centerMode: false,
+  vertical: true,
+  arrows: false,
+  focusOnSelect: true,
+  speed: 1500,
+  cssEase: 'linear'
+});
+
+let dropdown = document.querySelector('.dropdown');
+dropdown.onclick = function () {
+  dropdown.classList.toggle('active');
+}
+
+function show(a) {
+  document.querySelector('.text02').value = a;
+}
+
+
 const buttonClose = document.querySelector('.header-top__close');
 const headerDelivery = document.querySelector('.header-top');
 buttonClose.addEventListener('click', () => {
@@ -8,3 +38,8 @@ const link = document.querySelector('.home-banner__view')
 link.addEventListener('click', function name(e) {
   e.preventDefault();
 })
+
+
+
+
+
